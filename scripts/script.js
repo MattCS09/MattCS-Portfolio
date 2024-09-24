2c58fb1c686dfd4ffe3d816414e16e363195e87e
MattCastUCM <mattcsquintapvtacuenta@gmail.com>
2024-09-24 05:16:32 +0200

    improved responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     setLanguage();
+    initializeSlideshows();
 };
 
 function setLanguage() {
@@ -35,3 +36,26 @@ function toggleLang() {
     setLanguage();
 }
 
+
+// Fill with as many elements as slideshows in the page
+var slideIndex = [1, 1, 1];
+var slideId = ["slides1", "slides2", "slides3"]
+
+function initializeSlideshows() {
+    for(let i = 0; i < slideId.length; i++) showSlides(1, i)
+}
+
+function changeSlide(slideshowNum, slideNum) {
+    showSlides(slideIndex[slideNum] += slideshowNum, slideNum);
+}
+
+function showSlides(slideshowNum, slideNum) {
+    var i;
+    var x = document.getElementsByClassName(slideId[slideNum]);
+    if (slideshowNum > x.length) { slideIndex[slideNum] = 1 }
+    if (slideshowNum < 1) { slideIndex[slideNum] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[slideNum] - 1].style.display = "block";
+}
